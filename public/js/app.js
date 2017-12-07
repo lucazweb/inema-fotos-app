f47153a46091bde1025f10f61d236ae4478358b7
lucazweb <lucas.design@globo.com>
2017-12-06 21:31:28 -0300

    Implementando o Angular + mod. ngFileUpload + ajustes

---
+++ public/js/app.js
@@ -0,0 +1,27 @@
+angular.module('FotoApp', ['ngFileUpload'])
+    .controller('mainCtrl', function($scope, $http, Upload){
+        const endpoint = "http://localhost/inemaapi/aguas/api/data.php";
+        $scope.enviar = function(foto, file){
+            console.log(foto);
+            console.log(file);
+
+            Upload.upload({
+                url: endpoint,
+                file: file,
+                data: {
+                        colaborador: foto.colaborador,
+                        fotoNome: foto.nome,
+                        fotoLocal: foto.local,
+                        fotoData: foto.data
+                    }
+            }).then(function (resp) {
+                console.log(resp);
+                    //console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+                }, function (err) {
+                    console.log(err);
+                    //console.log('Error status: ' + resp.status);
+                });            
+
+        };
+   
+    });
