155ad2d43a543d3b9956e1ed4745e500954843c2
lucazweb <lucas.design@globo.com>
2017-12-08 16:44:09 -0300

    Ajustes finais

---
+++ sql/insert_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO `fotos` (`colaborador`, `foto_nome`, `foto_local`, `foto_data_registro`, `url`) VALUES
+('Lucas C. Santos', 'Águas de Itapuã', 'Itapuã, Salvador', '08122017', 'fotos/Itapua.jpg'),
+('Everaldo Pinho Santos', 'Por do Sol na Barra', 'Porto da Barra, Salvador', '08112016', 'fotos/porto_por.jpg');
